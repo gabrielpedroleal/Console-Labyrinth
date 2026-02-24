82e1343f9bc39e78c464a68943cb63ac4aa8cbb6
Gabriel Leal <gabriel-pedro-leal@hotmail.com>
2026-02-24 10:11:02 -0300

    Finished the menu, added 4 levels on a char[,] list, also made some improvemnts to the aesthetics

--- obj/Debug/net10.0/Console Labyrinth.AssemblyInfo.cs
+++ obj/Debug/net10.0/Console Labyrinth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console Labyrinth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e43fde73ff0a9a73c76a5e9fcc5610767942ec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ea9e0e4949c07b603f7d431166614e4b37c152")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console Labyrinth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console Labyrinth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
